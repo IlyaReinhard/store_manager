32513e80ceb353c0f1ebfed87a50ab625801d22d
Elijah <reinhardelijah1@gmail.com>
2022-02-28 18:55:08 +0300

    modified (file) create model of product

--- store_manager/settings.py
+++ store_manager/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps
+    'store_mng.apps.StoreMngConfig'
 ]
 
 MIDDLEWARE = [
--- store_mng/models.py
+++ store_mng/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Product(models.Model):
+    title_of_product = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    count_kg_product = models.FloatField(verbose_name='Количество(кг\л)')
+    price_of_kg = models.FloatField(verbose_name='Цена (руб) за 1 кг(л)')
+    making_date = models.DateField(null=True, blank=True, verbose_name='Дата изготовления')
+    expiration_date = models.SmallIntegerField(null=True, blank=True, verbose_name='Срок годности (количество суток)')
+
+    class Meta:
+        verbose_name_plural = 'Продукты'
+
+
+    def __str__(self):
+        return self.title_of_product
