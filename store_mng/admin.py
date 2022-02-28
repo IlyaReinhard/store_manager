a9190f8ce330e82d24b74f17a52313e6746a3ed2
Elijah <reinhardelijah1@gmail.com>
2022-02-28 21:33:37 +0300

    add(file) add new functionality for models

--- store_mng/admin.py
+++ store_mng/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title_of_product','count_kg_product','price_of_kg','making_date', 'expiration_date', 'produced_before')
--- store_mng/models.py
+++ store_mng/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import datetime
+from datetime import timedelta, datetime
 
 class Product(models.Model):
     title_of_product = models.CharField(max_length=100, verbose_name='Наименование продукта')
@@ -8,9 +8,20 @@ class Product(models.Model):
     making_date = models.DateField(null=True, blank=True, verbose_name='Дата изготовления')
     expiration_date = models.SmallIntegerField(null=True, blank=True, verbose_name='Срок годности (количество суток)')
 
+
+    @property
+    def produced_before(self):
+        """ Return date before product produced"""
+        expire_days = timedelta(days=self.expiration_date)
+        date_of_all = self.making_date + expire_days
+        return date_of_all
+
+    
+
     class Meta:
         verbose_name_plural = 'Продукты'
 
 
     def __str__(self):
         return self.title_of_product
+
